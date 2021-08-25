194e30ea35f3d9c2655a973acc9a0a77f5d8a810
Macsik121 <hiomimipad@gmail.com>
2021-08-25 18:09:02 +0300

    trying add cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 const db = require('./db');
 const installHandler = require('./api_handler');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 installHandler(app);
 
